8a3dc825e617661b9d921172fc79a0b58bf7aef0
onlyone <onlyone@onlyonedeMacBook-Pro.local>
2020-04-23 11:59:58 +0800

    aa

---
+++ src/main/java/com/onlyone/es/config/ElasticSearchTemplate.java
@@ -0,0 +1,81 @@
+package com.onlyone.es.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class ElasticSearchTemplate {
+    @Value("${elasticsearch.ip}")
+    private String ip;
+    @Value("${elasticsearch.port}")
+    private int port;
+    @Value("${elasticsearch.user}")
+    private String user;
+    @Value("${elasticsearch.password}")
+    private String password;
+    private RestHighLevelClient client;
+    @Value("${elasticsearch.connectTimeout}")
+    private int connectTimeout;
+    @Value("${elasticsearch.socketTimeout}")
+    private int socketTimeout;
+    @Value("${elasticsearch.connectionRequestTimeout}")
+    private int connectionRequestTimeout;
+    @Value("${elasticsearch.maxConnTotal}")
+    private int maxConnTotal;
+    @Value("${elasticsearch.maxConnPerRoute}")
+    private int maxConnPerRoute;
+    @Value("${elasticsearch.maxRetryTimeoutMillis}")
+    private int maxRetryTimeoutMillis;
+
+
+    /**
+     * 初始化RestHighLevelClient
+     */
+    @PostConstruct
+    public void init() {
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(user, password));
+        //配置化
+        RestClientBuilder builder =
+                RestClient.builder(new HttpHost(ip, port, "http")).setRequestConfigCallback(requestConfigBuilder -> {
+                    requestConfigBuilder.setConnectTimeout(connectTimeout);
+                    requestConfigBuilder.setSocketTimeout(socketTimeout);
+                    requestConfigBuilder.setConnectionRequestTimeout(connectionRequestTimeout);
+                    return requestConfigBuilder;
+                }).setHttpClientConfigCallback(httpClientBuilder -> {
+                    httpClientBuilder.setMaxConnTotal(maxConnTotal);
+                    httpClientBuilder.setMaxConnPerRoute(maxConnPerRoute);
+                    httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                    return httpClientBuilder;
+                }).setMaxRetryTimeoutMillis(maxRetryTimeoutMillis);
+
+        client = new RestHighLevelClient(builder);
+    }
+
+    public RestHighLevelClient getClient() {
+        return client;
+    }
+
+
+    @PreDestroy
+    public void destroy() throws IOException {
+        if (null != client) {
+            client.close();
+        }
+    }
+
+}
--- src/test/java/com/boot/Ch1ApplicationTest.java
+++ src/test/java/com/boot/Ch1ApplicationTest.java
@@ -1,15 +1,10 @@
 package com.boot;
 
-import com.boot.ch1.Application;
-import com.boot.ch1.Car;
-import com.boot.ch1.Driver;
+import com.onlyone.es.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.boot.ch1.MyConfig;
 
 /**
  * @author onlyone
@@ -18,26 +13,10 @@ import com.boot.ch1.MyConfig;
 @SpringBootTest(classes = Application.class)
 public class Ch1ApplicationTest {
 
-    @Autowired
-    private Car      car;
-
-    @Autowired
-    private Driver   driver;
-
-    @Autowired
-    private MyConfig myConfig;
 
     @Test
     public void contextLoads() {
-        boolean result = driver.getCar() == car;
-        System.out.println(result ? "同一个car" : "不同的car");
-
-        System.out.println("driver实例中的car：" + driver.getCar());
-        System.out.println("spring容器car：" + car);
 
     }
-    // 结果：
-    // 不同的car
-    // Car(id=1, name=car, version=3)
 
 }
